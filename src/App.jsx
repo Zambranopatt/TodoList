affa6ec6167ff8816b46e1d9a92ab3faa2f91b4d
Patrick Zambrano <patrickzambrano@gmail.com>
2025-09-26 16:03:37 +0800

    ready to push

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Todo from "./components/Todo";
+const App = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <Todo />
+    </section>
+  );
+};
+
+export default App;
